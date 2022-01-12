53f6b111ccd8aeb9051fcf59d658f532f7a9c497
+ <+>
2022-01-12 10:31:50 -0500

    commit tree 9b82807232f8e1cf8cb0d88978e692448428288e

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 5, 'M', 'brown', 'nice', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'M', 'black', 'spicy', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 2, 'F', 'polar', 'playful', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 7, 'M', 'grizzly', 'mean', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 4, 'F', 'grizzly', 'nice', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 9, 'M', 'sun', 'playful', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 6, 'F', 'panda', 'fun', 1);
+INSERT INTO bears VALUES (8, NULL, 4, 'M', 'panda', NULL, 1);
